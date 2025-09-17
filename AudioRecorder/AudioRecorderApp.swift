3b97a86250a328c5b9df35d677b5e656d7f18f39
Andrii Sabinin <sabinin.andrii@gmail.com>
2025-09-17 16:29:51 +0200

    Initial Commit

---
+++ AudioRecorder/AudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderApp.swift
+//  AudioRecorder
+//
+//  Created by Andrii Sabinin on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
